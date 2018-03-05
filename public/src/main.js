a17cac9be88c27dca9acf334c8573e003a6f2433
Trevor <trevor.spielman@gmail.com>
2018-03-05 16:13:54 -0700

    login page changes made.

--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,10 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// @ts-ignore
 import App from './App'
 import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
