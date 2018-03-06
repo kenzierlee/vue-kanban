d4a1a0a4a4fc353196b8d09a071d4520efb752f6
Trevor <trevor.spielman@gmail.com>
2018-03-06 12:13:29 -0700

    small changes made to tasks.

--- public/src/components/Tasks.vue
+++ public/src/components/Tasks.vue
@@ -1,48 +1,46 @@
 <template>
-    <div class="tasks">
-          <div class="card">
-              <div class="card-header">
-                <h5>{{task.title}}</h5>
-              </div>
-              <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-                <div v-for="comment in comments">
-                  <p>{{comment.comment}}</p>
-                  <p>user.name</p>
-                  <!-- add comment form -->
-                </div>
-              </div>
-            </div>
+  <div class="tasks">
+    <div class="card">
+      <div class="card-header">
+        <h5>{{task.title}}</h5>
+      </div>
+      <div class="card-body">
+        <p>{{task.description}}</p>
+        <div v-for="comment in comments">
+          <p>{{user.name}}</p>
+          <p>{{comment.comment}}</p>
+          <!-- add comment form -->
+        </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-    export default {
-      name: 'Tasks',
-      mounted(){
-      },
-      data() {
-        return {
-  
-        }
-  
-      },
-      methods: {
+  </div>
+  </div>
+</template>
 
+<script>
+  export default {
+    name: 'Tasks',
+    mounted() {
+    },
+    data() {
+      return {
+
+      }
+
+    },
+    methods: {
+
+    },
+    computed: {
+      user() {
+        return this.$store.state.user
       },
-      computed: {
-        user() {
-          return this.$store.state.user
-        },
-        tasks(){
-          return this.$store.state.tasks
-        }
+      tasks() {
+        return this.$store.state.tasks
       }
     }
-  </script>
-  
-  <style scoped>
-  </style>
+  }
+</script>
+
+<style scoped>
+</style>
--- server/models/task.js
+++ server/models/task.js
@@ -5,7 +5,7 @@ var schemaName = 'Task';
 
 var schema = new Schema({
     title: {type: String, required: true},
-    description: {type: String},
+    description: {type: String, required: true},
     userId: {type: ObjectId, ref: 'User'},
     boardId: {type: ObjectId, ref: 'Board'},
     membersId: [{type: ObjectId, ref: 'User'}],
