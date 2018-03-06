5f471a706e5f4b7bd77715bd1d27343b0a244859
Trevor <trevor.spielman@gmail.com>
2018-03-06 15:40:24 -0700

    small change in Lists. just need to commit.

--- public/src/components/Lists.vue
+++ public/src/components/Lists.vue
@@ -1,59 +1,60 @@
 <template>
   <div class="lists">
     <div>
-      <h3>{{list.title}}</h3><i class="fas fa-times-circle" @click="deleteList(list)"></i>
+      <h3>{{list.title}}</h3>
+      <i class="fas fa-times-circle" @click="deleteList(list)"></i>
       <p>
         <strong>{{list.description}}</strong>
       </p>
       <div v-for="task in tasks">
-        <Tasks></Tasks>
+       {{task.title}}
       </div>
       <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                aria-expanded="false">
-                Add A Task
-            </button>
-            <div class="dropdown-menu">
-                <form class="px-4 py-3" @submit.prevent="createTask">
-                    <div class="form-group">
-                        <label for="task-title">Title</label>
-                        <input v-model="task.title" type="text" class="form-control" id="task-title" placeholder="Title">
-                    </div>
-                    <div class="form-group">
-                        <label for="task-description">Description</label>
-                        <input v-model="task.description" type="text" class="form-control" id="task-description" placeholder="Description">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Add Task</button>
-                </form>
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
+          aria-expanded="false">
+          Add A Task
+        </button>
+        <div class="dropdown-menu">
+          <form class="px-4 py-3" @submit.prevent="createTask">
+            <div class="form-group">
+              <label for="task-title">Title</label>
+              <input v-model="task.title" type="text" class="form-control" id="task-title" placeholder="Title">
             </div>
+            <div class="form-group">
+              <label for="task-description">Description</label>
+              <input v-model="task.description" type="text" class="form-control" id="task-description" placeholder="Description">
+            </div>
+            <button type="submit" class="btn btn-primary">Add Task</button>
+          </form>
         </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-  import Task from './Tasks'
+  import Tasks from './Tasks'
   export default {
     name: 'Lists',
     props: ['list'],
-    // mounted() {
-    //   this.$store.dispatch('getTasks')
-    // },
+    mounted() {
+      // this.$store.dispatch('getTasks')
+    },
     data() {
       return {
-        task: {}
+        task: {
+          boardId: this.list.boardId,
+          listId: this.list._id
+        }
       }
-
     },
     methods: {
-        createTask(){
-            task.boardId = activeBoard._id;
-            task.listId = this.list._id;
-            this.$store.dispatch('createTask', this.task)
-        },
-        deleteList(list){
-            this.$store.dispatch('deleteList', list)
-        }
+      createTask(task) {
+        this.$store.dispatch('createTask', this.task)
+      },
+      deleteList(list) {
+        this.$store.dispatch('deleteList', list)
+      }
 
     },
     computed: {
@@ -63,8 +64,8 @@
       tasks() {
         return this.$store.state.tasks
       },
-      board(){
-          return this.$store.state.activeBoard
+      board() {
+        return this.$store.state.activeBoard
       }
     }
   }
